6fe827e1ccb0e1574fc016f2c53bd737ed447d6b
Payton Swick <payton@foolord.com>
2015-07-11 17:13:43 -0400

    Add basic input form

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,26 @@
+// Private functions
+function createHumblePressAdminBarButton() {
+	var button = document.createElement( 'li' );
+	button.className = 'wp-admin-bar-humblepress';
+	var buttonLink = document.createElement( 'a' );
+	buttonLink.className = 'ab-item';
+	buttonLink.href = '#';
+	buttonLink.appendChild( document.createTextNode( 'HumblePress' ) );
+	button.appendChild( buttonLink );
+	return button;
+}
+
+function createHumblePressForm() {
+	var formArea = document.createElement( 'div' );
+	formArea.id = 'humblepress-form';
+	var textArea = document.createElement( 'textarea' );
+	formArea.appendChild( textArea );
+	var submitButton = document.createElement( 'button' );
+	submitButton.appendChild( document.createTextNode( 'Post it!' ) );
+	formArea.appendChild( submitButton );
+	return formArea;
+}
+
 var humblePressLoader = {
 	addActivationButton: function() {
 		var adminBar = document.querySelector( '#wp-admin-bar-root-default' );
@@ -5,21 +28,36 @@ var humblePressLoader = {
 			console.error( 'HumblePress error: could not find admin bar' );
 			return;
 		}
-		var button = document.createElement( 'li' );
-		button.className = 'wp-admin-bar-humblepress';
-		var buttonLink = document.createElement( 'a' );
-		buttonLink.className = 'ab-item';
-		buttonLink.href = '#';
-		buttonLink.appendChild( document.createTextNode( 'HumblePress' ) );
-		button.appendChild( buttonLink );
+		var button = createHumblePressAdminBarButton();
 		adminBar.appendChild( button );
 		button.addEventListener( 'click', function() {
-			humblePressLoader.renderFormToPage();
+			humblePressLoader.toggleForm();
 		} );
 	},
 
+	toggleForm: function() {
+		var existingForm = document.querySelector( '#humblepress-form' );
+		if ( existingForm ) {
+			return humblePressLoader.removeForm();
+		}
+		humblePressLoader.renderFormToPage();
+	},
+
 	renderFormToPage: function() {
-		console.log( 'here' );
+		var body = document.querySelector( 'body' );
+		if ( ! body ) {
+			console.error( 'HumblePress error: could not find page body' );
+			return;
+		}
+		var form = createHumblePressForm();
+		body.insertBefore( form, body.firstChild );
+	},
+
+	removeForm: function() {
+		var existingForm = document.querySelector( '#humblepress-form' );
+		if ( existingForm && existingForm.parentNode ) {
+			existingForm.parentNode.removeChild( existingForm );
+		}
 	}
 };
 
